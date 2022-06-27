e9db9c48ce8e80d2ace458d032d64c7424852225
Lucas Martins <93499789+HexMagnus@users.noreply.github.com>
2022-06-27 19:54:58 -0300

    Atualização Aula_05
    Atualização da aula

---
+++ DeAlunoPraAluno/src/aulas/Aula_05.java
@@ -0,0 +1,29 @@
+package aulas;
+
+public class Aula_05 {
+
+	public static void main(String[] args) {
+	
+		int x;
+		x = 150;
+		
+		int y = 200;
+		
+		int soma = x+y;
+		
+		System.out.println(soma);
+
+	}
+
+}
+
+/* A chamada poderia ser:
+ * 
+ * System.out.println(x + y)
+ * ou atribuição da variavel (soma) a valores inteiros de x e y;
+ * System.out.println(x+" "+y) irá imprimir com espaço no meio
+ * System.out.println(x++y) irá concatenar
+ *
+ * 
+ */
+
