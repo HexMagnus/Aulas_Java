8f347e4355ed2d5a64791c26e10e4bcb6094645a
Lucas Martins <93499789+HexMagnus@users.noreply.github.com>
2022-06-28 21:17:51 -0300

    Atualização de aprendizado
    Atualização de aprendizado

---
+++ DeAlunoPraAluno/src/aulas/Aula_06.java
@@ -0,0 +1,20 @@
+package aulas;
+
+public class Aula_06 {
+
+	public static void main(String[] args) {
+		
+		int x;
+		x = 150;
+		
+		double y = 4;
+		double resultado = x/y;
+		
+		System.out.println(resultado);
+
+	}
+
+}
+
+/*se for feita divisão sem ser double, inteiro com inteiro irá gerar resultado inteiro.
+*/
---
+++ DeAlunoPraAluno/src/aulas/Aula_07.java
@@ -0,0 +1,26 @@
+package aulas;
+
+public class Aula_07 {
+
+	public static void main(String[] args) {
+		// Dados primitivos
+		
+		//Armazenamento de valores inteiros.
+		byte a;
+		short b;
+		int c;
+		long d;
+		
+		//Armazenamento de valores decimais.
+		float e;
+		double f;
+		
+		//Armazenamento de valores falso ou verdadeiro e caracteres.
+		boolean g;
+		char h;
+		
+		
+
+	}
+
+}
