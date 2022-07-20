3b7b7ca8ea758a6bf359e5998e6da7e09e8ba889
Lucas Martins <93499789+HexMagnus@users.noreply.github.com>
2022-07-20 20:18:46 -0300

    Update 2

---
+++ DeAlunoPraAluno/src/aulas/Aula_14.java
@@ -0,0 +1,22 @@
+package aulas;
+import java.util.Scanner;	
+
+
+public class Aula_14 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner input = new Scanner (System.in);
+		
+		System.out.println("Insira um valor em gramas");
+		int gramas = input.nextInt();
+		
+		int kilos = gramas /1000;
+		gramas = gramas % 1000;
+				
+		System.out.println("O total de Kilos é = "+kilos);
+		System.out.println("O total de gramas é=" +gramas);
+	}
+
+}
