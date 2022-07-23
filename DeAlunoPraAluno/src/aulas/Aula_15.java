6cdeab39c880bce8d31504f546d455c7b6ca298d
Lucas Martins <93499789+HexMagnus@users.noreply.github.com>
2022-07-22 23:28:20 -0300

    Update

--- DeAlunoPraAluno/src/aulas/Aula_15.java
+++ DeAlunoPraAluno/src/aulas/Aula_15.java
@@ -5,8 +5,18 @@ public class Aula_15 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		long totalMilisegundos = System.currentTimeMillis() -10800000;
 		
+		long totalSegundos = totalMilisegundos/1000;
+		long segundoAtual = totalSegundos %60;
 		
+		long totalMinutos = totalSegundos /60;
+		long minutoAtual = totalMinutos %60;
+		
+		long totalHora = totalMinutos /60;
+		long horaAtual = totalHora % 24;
+		
+		System.out.println(horaAtual+":"+minutoAtual+":"+segundoAtual);
 		
 	}
 
