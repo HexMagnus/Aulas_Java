174007a3d9cefc52abad5f8fe043ce660d5c5401
Lucas Martins <93499789+HexMagnus@users.noreply.github.com>
2022-07-27 20:40:42 -0300

    Update

--- DeAlunoPraAluno/src/aulas/Aula_19.java
+++ DeAlunoPraAluno/src/aulas/Aula_19.java
@@ -1,10 +1,31 @@
 package aulas;
 
+import java.util.Scanner;
+
 public class Aula_19 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Qual é o seu nome?");
+		String nome = input.nextLine();
+		
+		System.out.println("Qual é a sua idade");
+		int idade = input.nextInt();
+		
+		
+		if (idade <1) {
+			System.out.println("Idade informada não é válida");
+		}else if(idade >=18){
+			System.out.println(nome + " é maior de idade");
+			
+		}else {
+			System.out.println(nome + " é maior de idade");
+		}
+		
+		
 	}
 
 }
